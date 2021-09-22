97999fa6475242e940cd88c6909c754f39970e96
Dylan Eckhart <deckhart@roc-dev.com>
2021-09-22 14:49:39 +0200

    Version 1.21: Imageslider is fixed and given width/height

--- Foto's/script.js
+++ Foto's/script.js
@@ -8,7 +8,7 @@ function hideMenu() {
     navLinks.style.right = "-200px";
 }
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -20,9 +20,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
